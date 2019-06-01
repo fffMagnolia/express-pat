02f4cbdd3bb0cfe405e69ce16b7180a632f1b865
Magnolia <magnolia1925pa@gmail.com>
2019-06-02 01:41:26 +0900

    ・itemsのCUDを実装（Rは使用しないので未実装) ・DELETEは一括削除と単体削除を実装した

--- routes/items.js
+++ routes/items.js
@@ -13,20 +13,50 @@ let urlencodedParser = bodyParser.urlencoded({ extended: false })
 let jsonParser = bodyParser.json();
 
 let createItem = (req, res) => {
-  let item = db.items.build({ body: req.body.body, list_id: req.body.list_id });
+  let item = db.items.build({ body: req.body.item, list_id: req.body.list_id });
   item.save().then((result) => {
     res.json(result);
     res.end();
   })
 }
 
+let updateItem = (req, res) => {
+  let item = db.items.findOne({ where: { id: req.params['itemId'] } })
+    .then((item) => {
+      //TODO:body,stateのどちらかしか変更されない場合に処理わけしたい
+      item.update({ body: req.body.body, state: req.body.state })
+      item.save().then((result) => {
+        res.json(result);
+      })
+    })
+}
+
+let deleteItem = (req, res) => {
+  db.items.findOne({ where: { id: req.params['itemId'] } })
+    .then((item) => {
+      item.destroy();
+      res.end();
+    })
+}
+
+/* リストに紐づいたアイテムを一括削除する */
+let deleteItems = (req, res) => {
+  db.items.findAll({ where: { list_id: req.params['listId'] } })
+    .then((items) => {
+      items.destroy();
+      res.end();
+    })
+}
+
 /* 
  * localhost:3000/items/ からの相対パスで指定
  * getは多分使わないので封印
+ * postとputの:listIdはルーティングをわかりやすくするため入れている
 */
-router.post('/', jsonParser, createItem);
+router.post('/:listId', jsonParser, createItem);
 //router.get('/', getList);
-//router.put('/:listId/:itemId', jsonParser, updateListItem);
-//router.delete('/:listId', deleteItems);
+router.put('/:listId/:itemId', jsonParser, updateItem);
+router.delete('/:listId/:itemId', deleteItem);
+router.delete('/:listId', deleteItems);
 
 module.exports = router;
